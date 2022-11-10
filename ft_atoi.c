314d13661e0a81138bf6eb3ebbc562c1ab317819
Homma Rinka <hrinka@c1r23s5.42tokyo.jp>
2022-11-10 22:32:09 +0900

    1110

---
+++ ft_atoi.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hrinka <hrinka@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/10 21:06:30 by hrinka            #+#    #+#             */
+/*   Updated: 2022/11/10 22:07:15 by hrinka           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char	*str)
+{
+	int	num;
+	int	i;
+	int	neg;
+
+	num = 0;
+	i = 0;
+	neg = 1;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
+		|| str[i] == '\f' || str[i] == '\r')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			neg *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num = num * 10 + (str[i] - 48);
+		i++;
+	}
+	return (num * neg);
+}
+
+// int	main(void)
+// {
+// 	char	str[]= "-153678";
+
+// 	printf ("%d\n", ft_atoi(str));
+// 	printf ("%d\n", atoi (str));
+// }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hrinka <hrinka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 21:52:46 by hrinka            #+#    #+#             */
-/*   Updated: 2022/11/10 20:30:27 by hrinka           ###   ########.fr       */
+/*   Updated: 2022/11/10 20:36:16 by hrinka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,9 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int		ft_atoi(const char *str);
+
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s1);
 
 #endif
